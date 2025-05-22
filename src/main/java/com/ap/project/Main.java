536a8b71a09a315698932f904e56a04ce49aa660
Melika Ghasemipour <melika.ghasemipour.1385@gmail.com>
2025-05-23 01:30:29 +0330

    HttpServer created and added register endpoint added

--- src/main/java/com/ap/project/Main.java
+++ src/main/java/com/ap/project/Main.java
@@ -1,7 +1,24 @@
 package com.ap.project;
 
+import com.ap.project.httpHandler.RegisterHttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        int port = 8000;
+        String host = "localhost";
+
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+
+        httpServer.createContext("/auth/register", new RegisterHttpHandler());
+        /* TODO: Write Other endpoints */
+
+        httpServer.setExecutor(null);
+        httpServer.start();
+        System.out.println("Server started at port " + port);
 
         }
     }
