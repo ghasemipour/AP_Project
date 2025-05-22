6b0cf4c3599d6dc80f8d57fac7418e3fec18d830
Melika Ghasemipour <melika.ghasemipour.1385@gmail.com>
2025-05-23 01:34:12 +0330

    RegisterDao and BankAccountDao created

---
+++ src/main/java/com/ap/project/dao/BankAccountDao.java
@@ -0,0 +1,6 @@
+package com.ap.project.dao;
+
+public class BankAccountDao {
+    String bankName;
+    String accountNumber;
+}
---
+++ src/main/java/com/ap/project/dao/RegisterDao.java
@@ -0,0 +1,15 @@
+package com.ap.project.dao;
+
+import com.ap.project.Enums.UserRole;
+import com.ap.project.entity.general.BankAccount;
+
+public class RegisterDao {
+    String name;
+    String phoneNumber;
+    String password;
+    UserRole role;
+    String address;
+    String profileImageBase64;
+    BankAccount bankAccount;
+
+}
