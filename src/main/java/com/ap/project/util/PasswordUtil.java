c01ff861612ba5414b1267f1faf580e0a0d37b6b
Melika Ghasemipour <melika.ghasemipour.1385@gmail.com>
2025-05-23 20:52:20 +0330

    PasswordUtil created

---
+++ src/main/java/com/ap/project/util/PasswordUtil.java
@@ -0,0 +1,21 @@
+package com.ap.project.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordUtil {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) {
+                sb.append(String.format("%02X", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Hash Algorithm Not Available.", e);
+        }
+
+    }
+}
