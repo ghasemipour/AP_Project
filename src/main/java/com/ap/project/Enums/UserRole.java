efb6423a6759c0c9ae9045a6d7ab9919cab1dc6c
Melika Ghasemipour <melika.ghasemipour.1385@gmail.com>
2025-05-24 00:45:37 +0330

    UserRole problem fixed

--- src/main/java/com/ap/project/Enums/UserRole.java
+++ src/main/java/com/ap/project/Enums/UserRole.java
@@ -5,6 +5,21 @@ public enum UserRole {
     SELLER("seller"),
     COURIER("courier");
 
+    private final String roleName;
     UserRole(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public static UserRole fromString(String roleName) {
+        for(UserRole role : values()) {
+            if(role.roleName.equals(roleName)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Invalid role name: " + roleName);
     }
 }
