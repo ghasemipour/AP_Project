d17f957e2424242d4e295c97ec2f588c832186ca
Melika Ghasemipour <melika.ghasemipour.1385@gmail.com>
2025-05-27 16:38:05 +0330

    BankAccount setter added

--- src/main/java/com/ap/project/dto/BankAccountDto.java
+++ src/main/java/com/ap/project/dto/BankAccountDto.java
@@ -10,4 +10,9 @@ public class BankAccountDto {
     private String account_number;
 
     public BankAccountDto() {}
+
+    public BankAccountDto(String bankName, String accountNumber) {
+        this.bank_name = bankName;
+        this.account_number = accountNumber;
+    }
 }
